841ff1c3a58d248ee7f517ae5bd903565e7bcb1a
eliseuramos93 <eliseu.mec@gmail.com>
2023-08-08 19:53:32 -0300

    Add Flight reference to Booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_08_224201) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_08_225206) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_08_224201) do
   create_table "bookings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "flight_id", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
   create_table "bookings_passengers", id: false, force: :cascade do |t|
@@ -50,6 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_08_224201) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "bookings", "flights"
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
   add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
