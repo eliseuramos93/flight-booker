38443ed09f6ec5781c130587a0378683b28cc2ec
eliseuramos93 <eliseu.mec@gmail.com>
2023-08-08 18:21:07 -0300

    Initial commit with readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+airport = Airport.create([
+  {name: 'Aeroporto Santos Dumont (RJ)', code: 'SDU'},
+  {name: 'Aeroporto Tom Jobim/Galeão (RJ)', code: 'GIG'},
+  {name: 'Aeroporto de Congonhas (SP)', code: 'CGH'},
+  {name: 'Aeroporto Internacional de Guarulhos (SP)', code: 'GRU'}
+])
