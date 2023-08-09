e4788265c801e5e3bbcb5171c9003dc99e4b85a6
eliseuramos93 <eliseu.mec@gmail.com>
2023-08-09 14:49:44 -0300

    Add Booking system to enable passengers to book flights

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,25 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @number_of_tickets = params[:number_of_tickets].to_i
+    @flight = Flight.find(params[:flight_id].to_i)
+    @number_of_tickets.times { @booking.passengers.build }
+  end
+
+  def create
+    @flight = Flight.find(booking_params[:flight_id].to_i)
+    @booking = @flight.bookings.build(booking_params)
+
+    if @booking.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def booking_params
+      params.require(:booking).permit(:flight_id, 
+          passengers_attributes: [:name, :email])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :flights, only: [:index]
+  resources :bookings, only: [:new, :create]
 
   root to: 'flights#index'
 end
